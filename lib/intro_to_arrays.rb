3eaa1c1323e21347a24ec2aef0dceea53303be19
patrickhsanders <patrickhsanders@gmail.com>
2015-08-15 11:46:58 -0400

    hurray

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,34 @@
+def instantiate_new_array
+	my_array = []
+	return my_array
+end
+
+def array_with_two_elements
+	my_array = ["cat", "cow"]
+	return my_array
+end
+
+def first_element(my_array)
+	return my_array[0]
+end
+
+def third_element(my_arr)
+	return my_arr[2]
+end
+
+def last_element(my_arr)
+	return my_arr[-1]
+end
+
+def first_element_with_array_methods(my_arr)
+	return my_arr.first
+end
+
+
+def last_element_with_array_methods(my_arr)
+	return my_arr.last
+end
+
+def length_of_array(my_arr)
+	return my_arr.length
+end
