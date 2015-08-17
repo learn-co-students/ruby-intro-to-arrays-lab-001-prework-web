f74dc062558e9ac553842fc615afd7f01890f59e
james-marshall314 <james.marshall314@gmail.com>
2015-08-17 11:39:51 -0400

    passing

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,36 @@
+def instantiate_new_array
+  array = []
+end
+
+def array_with_two_elements
+  array = [1,2]
+end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
+
+def first_element_with_array_methods(array)
+  item = array.shift
+  array.unshift item
+  item
+end
+
+def last_element_with_array_methods(array)
+  item = array.pop
+  array.push item
+  item
+end
+
+def length_of_array(array)
+  array.length
+end
+
