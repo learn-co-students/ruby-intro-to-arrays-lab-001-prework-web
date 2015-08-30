b7bc45e488a5c9a6d22d9e8a12d8bea22abedf61
matthewbillienyc <matthewbillienyc@gmail.com>
2015-08-30 14:58:33 -0400

    passing

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,39 @@
+def instantiate_new_array
+  @my_new_array = Array.new
+end
+
+def array_with_two_elements
+  @my_two_array = Array.new(2)
+end
+
+@taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+
+def first_element(array)
+  array[0]
+end
+
+def second_element(array)
+  array[1]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
+
+@south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+
+def first_element_with_array_methods(array)
+  array.first
+end
+
+def last_element_with_array_methods(array)
+  array.last
+end
+
+def length_of_array(array)
+  array.length
+end
