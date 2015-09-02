6c0b086c7cca63ff86934827fcdcf8b7ad05e35d
jmona789 <jmona789@gmail.com>
2015-09-02 13:36:26 -0400

    Done

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  new_array = []
+end
+
+def array_with_two_elements
+  new_array_two_elements = [2, 3]
+end
+
+def first_element(array)
+  @taylor_swift[0]
+end
+
+def third_element(array)
+  @taylor_swift[2]
+end
+
+def last_element(array)
+  @taylor_swift[3]
+end
+
+def first_element_with_array_methods(array)
+  @south_east_asia.shift
+end
+
+def last_element_with_array_methods(array)
+  @south_east_asia.pop
+end
+
+def length_of_array(array)
+  @programming_languages.length
+end
