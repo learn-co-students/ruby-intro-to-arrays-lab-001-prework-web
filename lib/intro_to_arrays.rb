1b3ea4e476d56af463d49269142a78d9e13cf1d4
adavid8686 <adavid8686@gmail.com>
2015-09-03 03:27:48 -0400

    done

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  new_array = []
+end
+
+def array_with_two_elements
+  two_elements = [1,1]
+end
+
+def first_element(some_array)
+  some_array[0]
+end
+
+def third_element(some_array)
+  some_array[2]
+end
+
+def last_element(some_array)
+  some_array[-1]
+end
+
+def first_element_with_array_methods(some_array)
+  first_element(some_array)
+end
+
+def last_element_with_array_methods(some_array)
+  last_element(some_array)
+end
+
+def length_of_array(some_array)
+  some_array.length
+end
