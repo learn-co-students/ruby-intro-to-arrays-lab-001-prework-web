09876e7d7a261de187d98151dbd18de6cc345ea5
eenymini <hhl262@nyu.edu>
2015-09-07 20:01:51 -0400

    Done

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,39 @@
+def instantiate_new_array 
+
+my_new_array = Array.new
+
+end
+
+def array_with_two_elements
+my_two_array = ["dog", "cat"]
+end
+
+def first_element(argument = 1)
+taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+taylor_swift [0]
+end
+
+def third_element(argument = 1)
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+taylor_swift [2]
+end
+
+def last_element(argument = 1)
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+taylor_swift [3]
+end
+
+def first_element_with_array_methods(south_east_asia)
+  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  south_east_asia[0]
+end
+
+def last_element_with_array_methods(last_country)
+  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  south_east_asia[3]
+end
+
+def length_of_array(length)
+  programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+  programming_languages.length
+end
