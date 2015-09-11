deffbccd09931fbf2826783f9b565a50f56325a8
moziff <matteoziff@gmail.com>
2015-09-11 11:37:12 -0400

    done with first arrays

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,33 @@
+def instantiate_new_array
+    array1=Array.new
+    return array1
+end
+
+def array_with_two_elements
+    array2=[1,2]
+    return array2
+end
+
+def first_element(arrays)
+    return arrays[0]
+end
+
+def third_element(arrays)
+    return arrays[2]
+    end
+    
+def last_element(arrays)
+        return arrays[-1]
+end
+    
+    def first_element_with_array_methods(arrays)
+        return arrays.first
+    end
+    
+    def last_element_with_array_methods(arrays)
+        return arrays.last
+    end
+    
+    def length_of_array(arrays)
+        return arrays.length
+    end
