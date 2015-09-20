cfb77948b7e8df826f3853b6cd2a76bedd6e8416
wifelette <leahsilber@gmail.com>
2015-09-20 16:35:26 -0700

    Passing

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  uno = []
+end
+
+def array_with_two_elements
+  dos = [1, 2]
+end
+
+def first_element(arrr)
+  return arrr[0]
+end
+
+def third_element(arrr)
+  return arrr[2]
+end
+
+def last_element(arrr)
+  return arrr[-1]
+end
+
+def first_element_with_array_methods(arrr)
+  return arrr.first
+end
+
+def last_element_with_array_methods(arrr)
+  return arrr.last
+end
+
+def length_of_array(arrr)
+  return arrr.length
+end
