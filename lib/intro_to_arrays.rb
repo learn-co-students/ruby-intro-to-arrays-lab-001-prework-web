12311d3db22e7ba5f3df74274534cb39ee92396b
ruchiramani <ruchiramani@gmail.com>
2015-09-23 01:13:36 -0400

    Done

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,37 @@
+def instantiate_new_array
+  array_1 = []
+return array_1
+end
+
+
+def array_with_two_elements 
+  array_2 = [1,2]
+return array_2
+end
+
+def first_element(arr)
+return arr[0]
+end
+
+def third_element(arr)
+  return arr[2]
+end
+
+def last_element(arr)
+  return arr[-1]
+end
+
+def first_element_with_array_methods(arr)
+  x = arr.shift
+  return x
+end
+
+def last_element_with_array_methods(arr)
+  x = arr.pop
+  return x
+end
+
+def length_of_array(arr)
+  return arr.length
+end
+
