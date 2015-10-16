dc825631f82247f9cdbe2a635ca7b5dbd81ca092
WilliamAG <william.a.gough@gmail.com>
2015-10-16 18:05:42 -0400

    Done

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,52 @@
+def instantiate_new_array
+  my_new_array =[]
+end
+instantiate_new_array
+
+def array_with_two_elements
+  my_two_array = ["one", "two"]
+end
+array_with_two_elements
+
+def first_element(index)
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  taylor_swift [0]
+end
+first_element(0)
+
+def third_element(index)
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  taylor_swift [2]
+end
+third_element(2)
+
+def last_element(index)
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  taylor_swift [3]
+end
+last_element(3)
+
+def first_element_with_array_methods(index)
+  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  south_east_asia.first 
+end
+first_element_with_array_methods(0)
+
+def last_element_with_array_methods(index)
+  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  south_east_asia.last
+end
+last_element_with_array_methods(3)
+
+def length_of_array(index)
+  programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+  programming_languages.length 
+end
+length_of_array(0)
+
+
+
+
+
+
+
