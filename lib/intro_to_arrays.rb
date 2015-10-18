0355c593ccac2f4b044d52f21f64c953017274cf
flats <dave@robador.com>
2015-10-17 20:34:54 -0400

    Gemfile.lock w/ new Rspec, write array methods

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,32 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  ["two", "elements"]
+end
+
+def first_element(elements)
+  elements[0]
+end
+
+def third_element(elements)
+  elements[2]
+end
+
+def last_element(elements)
+  elements[-1]
+end
+
+def first_element_with_array_methods(elements)
+  elements.shift
+end
+
+def last_element_with_array_methods(elements)
+  elements.pop
+end
+
+def length_of_array(elements)
+  elements.length
+end
+
