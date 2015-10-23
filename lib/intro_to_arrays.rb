0691d0897c2f44d75c474fa5619e5d2ed45c05cb
Micheldeouf <arnaud.frossard@gmail.com>
2015-10-23 11:00:11 +0200

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,66 @@
+def instantiate_new_array
+
+	@my_new_array = []
+	@my_new_array
+
+end
+
+def array_with_two_elements
+
+	@my_two_array = ["Element-1", "Element-2"]
+	@my_two_array
+
+end
+
+def first_element(x)
+
+	@taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+	x = 0
+	my_first_element = @taylor_swift[x]
+	my_first_element
+
+end
+
+def third_element(y)
+
+	@taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+	y = 2 
+	my_third_element = @taylor_swift[y]
+	my_third_element
+
+end
+
+def last_element(z)
+
+	@taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+	z = -1
+	my_last_element = @taylor_swift[z]
+	my_last_element
+
+end
+
+def first_element_with_array_methods(x)
+
+	@south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+	x = 0
+	element_1 = @south_east_asia.at(x)
+	element_1
+
+end
+
+def last_element_with_array_methods(position)
+
+	@south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+	position = 3
+	element_last = @south_east_asia.at(position)
+	element_last
+
+end
+
+def length_of_array(arr)
+
+	@programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+	arr = @programming_languages
+	arr.count
+
+end
