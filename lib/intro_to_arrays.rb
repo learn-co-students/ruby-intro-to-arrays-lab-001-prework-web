113314555790dd229783aeb803ea41a37b4d8032
github username <github email address>
2015-11-18 16:08:22 -0500

    finish

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,37 @@
+def instantiate_new_array
+  []
+end 
+
+def array_with_two_elements
+  ray = []
+  2.times{ |num| ray << num }
+  ray
+end 
+
+def first_element(taylor_swift)
+  taylor_swift.first
+end 
+
+def third_element(taylor_swift)
+  taylor_swift[2]
+end 
+
+def last_element(taylor_swift)
+  taylor_swift.last
+end 
+
+def first_element_with_array_methods(south_east_asia)
+  south_east_asia.last
+end 
+
+def last_element_with_array_methods(south_east_asia)
+  south_east_asia.last
+end 
+
+def first_element_with_array_methods(south_east_asia)
+  south_east_asia.first
+end 
+
+def length_of_array(programming_languages)
+  programming_languages.length
+end 
