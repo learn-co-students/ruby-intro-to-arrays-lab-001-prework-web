00980d1a28460d5ab0d7a7f59586aad728254fc9
cocacolabe <cocacolabear.la@gmail.com>
2015-11-24 00:06:32 -0500

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,40 @@
+def instantiate_new_array
+  instantiate_new_array = []
+
+  instantiate_new_array
+end
+
+def array_with_two_elements
+  gogo = ["gg", "oo"]
+  gogo
+end
+
+def first_element(gogo)
+  gogo = ["Welcome to New York", "oo"]
+  gogo[0]
+end
+
+def third_element(third)
+  third = ["gg","oo","Style"]
+  third[2]
+end
+
+def last_element(gogo)
+  gogo = ["gg", "Out of The Woods"]
+  gogo.last
+end
+
+def first_element_with_array_methods(gogo)
+  gogo = ["Thailand", "oo"]
+  gogo.first
+end
+
+def last_element_with_array_methods(gogo)
+  gogo = ["gg", "Myanmar"]
+  gogo.last
+end
+
+def length_of_array(gogo)
+  gogo = ["gg", "oo","G","I","N","N","E","!"]
+  gogo.length
+end
