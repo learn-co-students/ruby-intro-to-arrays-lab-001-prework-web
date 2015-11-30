0ac7abc9b694e6ac5c1552a038472d0ff6400f1b
OrthoStice <shelleyhicklin@hotmail.com>
2015-11-30 02:01:17 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,40 @@
+def instantiate_new_array
+  my_new_array=[]
+  my_new_array
+end
+
+def array_with_two_elements
+  my_two_array=["1","2"]
+  my_two_array
+end
+
+def first_element (array)
+  my_first_element = array
+  my_first_element[0]
+end
+
+def third_element (array)
+  my_third_element = array
+  my_third_element[2]
+end
+
+def last_element (array)
+  my_last_element = array
+  my_last_element[-1]
+end
+
+def first_element_with_array_methods(array)
+  first_country=array
+  first_country.first
+end
+
+def last_element_with_array_methods(array)
+  last_country=array
+  last_country.last
+end
+
+def length_of_array(array)
+  length=array 
+  length.count
+end
+
