a60aaee787bfe97937ed8ae120dcb3863e6b1628
lovelysusy924 <susana.fernandez.simontech.16@gmail.com>
2015-12-01 23:24:02 +0000

    Finished Intro to Arrays Lab

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,40 @@
+def instantiate_new_array
+  instantiate_new_array = []
+end 
+
+def array_with_two_elements
+  array_with_two_elements = ["susana", "fernandez"]
+end
+
+def indexing(college_choices)
+  college_choices = ["UC Irvine", "CSLB", "CSLA","Cal state Fresno"]
+end
+
+def first_element(college_choices)
+  college_choices[0]
+end
+
+def third_element(college_choices)
+  college_choices[2]
+end
+
+def last_element(college_choices)
+  college_choices[-1]
+end
+
+def first_element_with_array_methods1(south_east_asia)
+  south_east_asia = ["Thailand", "indonesia", "Singapore", "Malaysia"]
+end
+
+def first_element_with_array_methods(south_east_asia)
+  south_east_asia.first
+end
+
+def last_element_with_array_methods(south_east_asia)
+  south_east_asia.last
+end
+
+def length_of_array(programming_languages)
+  programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "HTML", "Idk", "cool"]
+programming_languages.count
+end
