9fc71c5e881dbc0c2dc2ba8455f1907ec5af9a31
mayra0701 <dmayra41@gmail.com>
2015-12-02 02:08:15 +0000

    Finished intro_to_arrays.rb

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,40 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  array_with_two_elements = ["juan", "pancho"]
+end
+
+def indexing(exotic_places)
+  exotic_places = ["South Africa", "Indonesia", "Bahamas","Belize"]
+end
+
+def first_element(exotic_places)
+  exotic_places[0]
+end
+
+def third_element(exotic_places)
+  exotic_places[2]
+end
+
+def last_element(exotic_places)
+  exotic_places[-1]
+end
+
+def first_element_with_array_methods1(south_east_asia)
+  south_east_asia = ["Nepal", "Haiti", "Panama", "Colombia"]
+end
+
+def first_element_with_array_methods(south_east_asia)
+  south_east_asia.first
+end
+
+def last_element_with_array_methods(south_east_asia)
+  south_east_asia.last
+end
+
+def length_of_array(the_perfect_breakfast)
+  the_perfect_breakfast = ["Flour", "Blueberries", "Bacon", "Eggs", "Strawberries", "margarine", "almond milk", "Wafflelaffeles"]
+  the_perfect_breakfast.count
+end
