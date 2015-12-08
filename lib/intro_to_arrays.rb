f92146daa17e5d7ec2dcc7ebbed41e1bdb77fdaa
dandanberry <dandanberry@gmail.com>
2015-12-08 13:15:22 -0800

    8 methods in intro_to_arrays

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array 
+	[]
+end
+
+def array_with_two_elements
+	array = ['a','b']
+end
+
+def first_element(a)
+	return a[0]
+end
+
+def third_element(a)
+	return a[2]
+end
+
+def last_element(a)
+	return a[-1]
+end
+
+def first_element_with_array_methods(a)
+	return a.first
+end
+
+def last_element_with_array_methods(a)
+	return a.last 
+end
+
+def length_of_array(a)
+	return a.length
+end
