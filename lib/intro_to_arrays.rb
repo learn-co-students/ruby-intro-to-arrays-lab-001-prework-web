54d19fc6cedd567612c857fcab6fca60a46875ee
Adam Hardy <rpihardy@gmail.com>
2015-12-13 23:17:37 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,39 @@
+def instantiate_new_array
+  my_new_array = []
+  my_new_array
+end
+
+def array_with_two_elements
+  my_two_array = ["cat", "dog"]
+  my_two_array
+end
+
+def first_element(array)
+  my_first_element = array[0]
+  my_first_element
+end
+
+def third_element(array)
+  my_third_element = array[2]
+  my_third_element
+end
+
+def last_element(array)
+  my_last_element = array[-1]
+  my_last_element
+end
+
+def first_element_with_array_methods(array)
+  first_country = array.first
+  first_country
+end
+
+def last_element_with_array_methods(array)
+  last_country = array.last
+  last_country
+end
+
+def length_of_array(array)
+  length = array.length
+  length
+end
