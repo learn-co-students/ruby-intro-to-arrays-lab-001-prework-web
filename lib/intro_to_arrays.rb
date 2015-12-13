89604f1661cdce8bedac4695009549f42589724c
ClReece <clreece@hotmail.com>
2015-12-12 21:07:05 -0500

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,34 @@
+def instantiate_new_array
+  []
+end
+  
+def array_with_two_elements
+  ["George","Glass"]
+end
+
+def first_element(tswift)
+ tswift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+ tswift [0]
+end
+
+def third_element(tswift)
+  tswift[2]
+end
+
+def last_element(tswift)
+  tswift[-1]
+end
+
+def first_element_with_array_methods(asia)
+  asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  asia.first 
+end
+
+def last_element_with_array_methods(asia)
+  asia.last
+end
+
+def length_of_array(computer_language)
+  computer_language = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+  computer_language.length 
+end
