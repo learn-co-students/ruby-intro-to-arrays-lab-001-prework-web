7e86bba90ead90c18399dd5722b60c0745d144ad
gregmarquet <marquet.greg@gmail.com>
2015-12-15 19:27:09 -0500

    Done

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,57 @@
+def instantiate_new_array
+  my_array = []
+  return my_array
+end
+
+def array_with_two_elements
+  new_array = [1, 2]
+  return new_array
+end
+
+def first_element(array)
+  return array.first
+end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  return array[-1]
+end
+
+def first_element_with_array_methods(array)
+  array.shift
+end
+
+def last_element_with_array_methods(array)
+  array.pop
+end
+
+def length_of_array(array)
+  array.length
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
