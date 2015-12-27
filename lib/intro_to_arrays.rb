39e0e76354622c8004b1827db4fc04cbf9cc474e
WilliamAG <william.a.gough@gmail.com>
2015-12-26 17:44:28 -0800

    done

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,52 +1,31 @@
 def instantiate_new_array
-  my_new_array =[]
+  a = []
 end
-instantiate_new_array
 
 def array_with_two_elements
-  my_two_array = ["one", "two"]
+  a = [1,2]
 end
-array_with_two_elements
 
-def first_element(index)
-  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  taylor_swift [0]
+def first_element(a)
+  a[0]
 end
-first_element(0)
 
-def third_element(index)
-  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  taylor_swift [2]
+def third_element(a)
+  a[2]
 end
-third_element(2)
 
-def last_element(index)
-  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  taylor_swift [3]
+def last_element(a)
+  a[-1]
 end
-last_element(3)
 
-def first_element_with_array_methods(index)
-  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
-  south_east_asia.first 
+def first_element_with_array_methods(a)
+  a.first 
 end
-first_element_with_array_methods(0)
 
-def last_element_with_array_methods(index)
-  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
-  south_east_asia.last
+def last_element_with_array_methods(a)
+  a.last
 end
-last_element_with_array_methods(3)
 
-def length_of_array(index)
-  programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
-  programming_languages.length 
+def length_of_array(a)
+  a.length
 end
-length_of_array(0)
-
-
-
-
-
-
-
