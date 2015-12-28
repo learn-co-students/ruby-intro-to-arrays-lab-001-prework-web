b84321e634edcbd3aa04246dad505ac7429b133b
franco84 <franco84@gmail.com>
2015-12-28 03:50:18 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  hi = []
+end
+
+def array_with_two_elements
+  hii = [1,2]
+end
+
+def first_element(array_with_two_elements)
+  array_with_two_elements[0]
+end
+
+def third_element(array_with_two_elements)
+  array_with_two_elements[2]
+end
+
+def last_element(array_with_two_elements)
+  array_with_two_elements[-1]
+end
+
+def first_element_with_array_methods(array_with_two_elements)
+  array_with_two_elements.first
+end
+
+def last_element_with_array_methods(array_with_two_elements)
+  array_with_two_elements.last
+end
+
+def length_of_array(array_with_two_elements)
+  array_with_two_elements.length
+end
