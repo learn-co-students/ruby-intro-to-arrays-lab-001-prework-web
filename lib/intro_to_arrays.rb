e6083c86475181606ec703dd5a5798962acbe5d0
Joan Manuel Araujo <WISDOMBTHEGAME@GMAIL.COM>
2016-01-31 15:33:30 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,41 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  new_array = ["element1", "element2"]
+end
+
+taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+
+def first_element(taylor_swift)
+  taylor_swift[0]
+end
+
+def third_element(taylor_swift)
+  taylor_swift[2]
+end
+
+def last_element(taylor_swift)
+  taylor_swift[3]
+end
+
+south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+
+def first_element_with_array_methods(south_east_asia)
+  south_east_asia.first
+end
+
+def last_element_with_array_methods(south_east_asia)
+  south_east_asia.last
+end
+
+programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+
+def length_of_array(programming_languages)
+  programming_languages.length
+end
+
+
+
+
