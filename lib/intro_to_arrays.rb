ca3e5fe6542185d764eaee8c1dff311bcadfa862
jakeekaj <jakemalto2014@gmail.com>
2016-02-10 17:30:14 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,32 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  [1,2]
+end
+
+def first_element(x)
+  x[0]
+end
+
+def third_element(x)
+  x[2]
+end
+
+def last_element(x)
+  x[-1]
+end
+
+def first_element_with_array_methods(x)
+  x.shift
+end
+
+def last_element_with_array_methods(x)
+  x.pop
+end
+
+def length_of_array(x)
+  x.length
+end
+
