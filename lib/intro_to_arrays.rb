379a6fb8f281fffc2125945ec32211493898f84b
jasonkwong11 <jasonkwong11@gmail.com>
2016-02-19 18:38:49 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,38 @@
+def instantiate_new_array
+  new_array =[]
+end
+
+def array_with_two_elements
+  new_array=["glasses", "hair"]
+end
+
+def first_element(my_first_element)
+  my_first_element = ["Welcome to New York", "glasses", "hair"]
+  my_first_element[0]
+end
+
+def third_element(my_third_element)
+  my_third_element = ["one", "two", "Style", "@taylor_swift"]
+  my_third_element[2]
+end
+
+def last_element(my_last_element)
+   my_last_element = ["one", "two", "Style", "@taylor_swift", "Out of The Woods"]
+  my_last_element[-1]
+end
+
+def first_element_with_array_methods(first_country)
+  first_country = ["Thailand"]
+  first_country.first
+end
+
+def last_element_with_array_methods(last_country)
+  last_country = ["Myanmar"]
+  last_country.last
+end
+
+def length_of_array(length)
+  length = [1,2,3,4,5,6,7,8]
+  length.length
+end
+
