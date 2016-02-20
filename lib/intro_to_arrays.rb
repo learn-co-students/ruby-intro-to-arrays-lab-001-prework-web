15aa4da87cca711246d6750c3abee5a599da3af5
CodeByLine <yleventhal@ymail.com>
2016-02-20 17:42:40 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,39 @@
+def instantiate_new_array
+ # my_new_array = array.new
+  my_new_array = []
+end
+
+def array_with_two_elements
+  my_new_array_w_two = []
+  my_new_array_w_two = ["sun", "moon"]
+end
+
+def first_element(my_new_array )
+  my_new_array[0]
+end
+
+def first_element_with_array_methods(my_new_array)
+ # my_new_array = ["sun", "moon"]
+  my_new_array.first
+end
+
+def third_element(my_new_array )
+  #my_new_array = ["sun", "moon", "stars", "comets"]
+  my_new_array[2]
+end
+
+def last_element(my_new_array )
+  my_new_array[-1]
+end
+
+def last_element_with_array_methods(my_new_array )
+  my_new_array.last
+end
+
+def length_of_array(my_new_array )
+  my_new_array.length
+  my_new_array.size
+  my_new_array.count
+end
+
+
