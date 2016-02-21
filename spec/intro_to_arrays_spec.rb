2bce39e1d6750ef64d84086c9193726a934c5047
klaubel <beltran.klaudia@hotmail.com>
2016-02-20 21:05:23 -0500

    Done.

--- spec/intro_to_arrays_spec.rb
+++ spec/intro_to_arrays_spec.rb
@@ -3,10 +3,7 @@ describe "working with arrays" do
 
   context 'instantiating' do
     describe "#instantiate_new_array" do
-      #the variables that begin with the '@' symbol are called instance variables. 
-      #We'll learn more about them when we get to object orientation. 
-      #For now, just know that creating an instance variable inside our describe block 
-      #means we can access that variable anywhere inside the block.
+    
       before(:each) do
         @my_new_array = instantiate_new_array
       end
@@ -90,7 +87,7 @@ describe "working with arrays" do
     end
 
     describe "#length_of_array" do
-      it 'takes in an argrument of an array and returns the length of the array' do
+      it 'takes in an argument of an array and returns the length of the array' do
         length = length_of_array(@programming_languages)
         expect(length).to eq(8)
       end
