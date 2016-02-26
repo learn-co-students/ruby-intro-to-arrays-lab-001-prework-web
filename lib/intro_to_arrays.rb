476ec3873749dcbdc025b44ae32875c09305f572
gnwankwo <gnwankwo03@gmail.com>
2016-02-26 15:15:05 -0500

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,49 @@
+
+def instantiate_new_array()
+  array= Array.new
+  array
+end
+  
+
+
+
+def array_with_two_elements()
+  array= Array.new
+  array.push(1,2)
+  array
+end
+
+def first_element(array)
+  array[0]
+end
+ #my_first_element = @taylor_swift[0]
+ 
+def third_element(array)
+  array[2]
+end
+
+
+def last_element(array)
+  array[-1]
+end
+
+
+ #@south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+
+def first_element_with_array_methods(array)
+  first=array.shift
+  first
+end
+
+
+def last_element_with_array_methods(array)
+  array.last 
+end
+
+def length_of_array(array)
+  array.length
+end
+
+#programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+#length= programming_languages.length
+#puts length 
