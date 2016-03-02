cf14d3f767c795ce06b87482f261778d387ca4da
dougcrisona <dougcrisona@gmail.com>
2016-03-02 11:40:25 -0500

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,33 @@
+def instantiate_new_array
+  @my_new_array = []
+end
+
+def array_with_two_elements
+  @my_two_array = [1, 2]
+end
+
+def first_element(index_array)
+  index_array = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  index_array[0]
+end
+
+def third_element(index_array)
+  index_array[2]
+end
+
+def last_element(index_array)
+  index_array[-1]
+end 
+
+def first_element_with_array_methods(array_methods)
+  array_methods.first
+end
+
+def last_element_with_array_methods(array_methods)
+  array_methods.last
+end
+
+def length_of_array(array_methods)
+  array_methods.length
+end
+
