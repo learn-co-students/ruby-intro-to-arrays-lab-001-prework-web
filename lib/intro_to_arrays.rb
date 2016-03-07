c3ec11a2c486d62a6f0624347b49fb386a08cb27
wengong <wentoshare@gmail.com>
2016-03-07 18:03:47 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,34 @@
+def instantiate_new_array
+
+  abc=Array.new()
+
+end
+def array_with_two_elements
+  abc=[1,2]
+end
+
+def first_element(abc)
+  abc.first
+end
+
+def third_element(abc)
+  abc[2]
+end
+
+def last_element(abc)
+  abc.last
+end
+
+def first_element_with_array_methods(abc)
+  abc.first
+end
+
+def last_element_with_array_methods(abc)
+  abc.last
+end
+
+
+def length_of_array(abc)
+  abc.length
+end
+
