87920dc1888b98f78642495404b8fdef998ef6b1
WerkerM <michael.handwerker@gmail.com>
2016-03-18 15:50:05 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  my_new_array = []
+end
+
+def array_with_two_elements
+  my_two_array = [1, 2]
+end
+
+def first_element(taylor_swift)
+  taylor_swift.first
+end
+
+def third_element(taylor_swift)
+  taylor_swift[2]
+end
+
+def last_element(taylor_swift)
+  taylor_swift.last
+end
+
+def first_element_with_array_methods(taylor_swift)
+  taylor_swift.first
+end
+
+def last_element_with_array_methods(taylor_swift)
+  taylor_swift.last
+end
+
+def length_of_array(taylor_swift)
+  taylor_swift.length
+end
