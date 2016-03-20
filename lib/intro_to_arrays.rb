f16b9b9fc7b6193758ab383bffc33fde541a2440
RHOPRHH <rhoprhh@gmail.com>
2016-03-20 17:05:35 -0400

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,45 @@
+def instantiate_new_array
+  x = []
+  x
+end
+
+def array_with_two_elements
+  y = [1,2]
+  y
+end
+
+def first_element(array)
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  my_first_element = array[0]
+  my_first_element
+end
+
+
+def third_element(array)
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  my_third_element = array[2]
+  my_third_element
+end
+
+def last_element(array)
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  my_last_element = array[3]
+  my_last_element
+end
+
+
+
+def first_element_with_array_methods(array)
+  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  first_country = array.first
+end
+
+def last_element_with_array_methods(array)
+  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  last_country = array.last
+end
+
+def length_of_array(array)
+  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  length_of_array = array.length
+end
