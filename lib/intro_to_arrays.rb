452d3f5cdbf7c44727dccfa03c141fc7008be38e
Steve Mann <sjmann15@gmail.com>
2016-03-26 19:01:08 +0000

    Completed.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,39 @@
+def instantiate_new_array
+  my_array = []
+  my_array
+end
+
+def array_with_two_elements
+  my_array = ["cat","dog"]
+  my_array
+end
+
+def first_element(array)
+  array = ["Welcome to New York"]
+  array[0]
+end
+
+def third_element(array)
+  array = ["Name","Age","Style"]
+  array[2]
+end
+
+def last_element(array)
+  array = ["Name","Age","Out of The Woods"]
+  array.last
+end
+
+def first_element_with_array_methods(array)
+  array = ["Thailand","Age","Thailand"]
+  array.first
+end
+
+def last_element_with_array_methods(array)
+  array = ["Name","Age","Myanmar"]
+  array.last
+end
+
+def length_of_array(array)
+  array = ["Name","Age","Myanmar",4,5,6,7,8]
+  array.count
+end
