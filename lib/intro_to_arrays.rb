32a69884d69d87e3c37b23a70ef4a2d890664c63
WigWog <andyzhao.zhao@gmail.com>
2016-04-02 20:51:48 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,36 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  array_2 = ["C9", "TSM"]
+end
+
+def first_element(x)
+  array_TS = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  first_element = array_TS.first
+end
+
+def third_element(x)
+  array_TS = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  third_element = array_TS[2]
+end
+
+def last_element(x)
+  array_TS = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  last_element = array_TS.last
+end
+
+def first_element_with_array_methods(x)
+  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  first_element_with_array_methods = south_east_asia.first
+end
+
+def last_element_with_array_methods(x)
+  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  last_elememt_with_array_methods = south_east_asia.last
+end
+
+def length_of_array(x)
+  length_of_array = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"].length
+end
