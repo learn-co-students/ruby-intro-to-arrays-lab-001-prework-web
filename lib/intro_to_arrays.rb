dd690da485695582af2d03105a155a7314cfe993
BettinaP <bettinaprophete@gmail.com>
2016-04-04 21:36:29 -0400

    ruby intro to arrays lab 2

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,37 +1,33 @@
 def instantiate_new_array
-my_new_array = []
+  @my_new_array = []
 end
 
 def array_with_two_elements
-  my_two_array = [1,2]
+ @my_two_array = [1,2]
 end
 
-def first_element(my_first_element)
-  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  taylor_swift[0]
+def first_element(array)
+  array[0]
 end
 
-def third_element(my_third_element)
-  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  taylor_swift[2]
+def third_element(array)
+  array[2]
 end
 
-def last_element(my_last_element)
-  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  taylor_swift[-1]
+def last_element(array)
+  array[-1] 
 end
 
-def first_element_with_array_methods(first_country)
-  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
-  south_east_asia.first 
+def first_element_with_array_methods(array)
+  array.first
 end
 
-def last_element_with_array_methods(last_country)
-  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
-  south_east_asia.last  
+def last_element_with_array_methods(array)
+  array.last
 end
 
-def length_of_array(length)
-  programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
-  programming_languages.length
+def length_of_array(array)
+  array.length 
 end
+
+
