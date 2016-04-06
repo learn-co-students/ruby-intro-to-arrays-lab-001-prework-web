e97911a63656537f17ca6873fe74fe20a5aa98d6
msrashid <wlitmilkyway@gmail.com>
2016-04-06 01:39:50 +0000

    practicing arrays

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,36 @@
+def instantiate_new_array
+[]
+return []
+end
+
+def array_with_two_elements
+  hello = ["one", "two"]
+  return hello
+end
+
+def first_element(arrayone)
+ return arrayone.first
+end
+
+def third_element(arrayone)
+  return arrayone[2]
+end
+
+def last_element(arrayone)
+  return arrayone[-1]
+  
+end
+
+def first_element_with_array_methods(arrayone)
+  return arrayone.first
+end
+
+def last_element_with_array_methods(arrayone)
+  return arrayone.last
+end
+
+def length_of_array(arrayone)
+  return arrayone.length
+  
+end
+
