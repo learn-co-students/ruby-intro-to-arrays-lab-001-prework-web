f0af9a58fb441839fffc852ffdeccc93060da7c3
iamaimeemo <aimeemosseri@gmail.com>
2016-04-17 01:27:50 -0400

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,39 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+@my_two_array = ["uno", "dos"]
+end
+
+def first_element (array)
+@taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+array.first
+end
+
+def third_element (array)
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  array[2]
+end
+
+def last_element (array)
+@taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+array.last
+end
+
+def first_element_with_array_methods (array)
+   @south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+array.first
+end
+
+def last_element_with_array_methods (array)
+  @south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  array.last
+  
+end
+
+def length_of_array (array)
+  @programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+  array.size
+  
+end
