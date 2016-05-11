9fbf5938b23b6ea02b3304ea7bd8f23352a6e2dd
Kevin Hernon <khernon1@gmail.com>
2016-05-10 20:34:43 -0400

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,43 @@
+def instantiate_new_array
+[]
+end
+
+def array_with_two_elements
+  arr = [1, 2]
+end
+
+def first_element(i)
+i = 0
+arr = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+arr[i]
+end
+
+def third_element(i)
+  i = 2
+  arr = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  arr[i]
+end
+
+def last_element(i)
+  i = -1
+  arr = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  arr[i]
+end
+
+def first_element_with_array_methods(i)
+  i = 0
+  arr = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  arr[i]
+end
+
+def last_element_with_array_methods(i)
+    i = -1
+    arr = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+    arr[i]
+  end
+
+  def length_of_array(i)
+    arr = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+    i = arr.length
+    i
+  end
