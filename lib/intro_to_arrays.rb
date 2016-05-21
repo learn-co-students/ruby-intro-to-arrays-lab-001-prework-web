30b2665f303009fa0f0dc6472e0f0dadc319d9e9
Gaytri Rabindranauth <gaytri.rab@gmail.com>
2016-05-20 22:44:57 -0400

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  instantiate_new_array = []
+end
+
+def array_with_two_elements
+  new_array = ["", ""]
+end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array.last
+end
+
+def first_element_with_array_methods(first_country)
+  first_country[0]
+end
+
+def last_element_with_array_methods(last_country)
+  last_country.last
+end
+
+def length_of_array(length)
+  length.length
+end
