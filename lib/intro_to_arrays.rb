ebc00a5fd8eab52049bc8273940ab8ca1646f315
Akhmet Safarov <akhmetsafarov1989@icloud.com>
2016-06-21 05:11:39 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  array = []
+end
+
+def array_with_two_elements
+  array = [1,2]
+end
+
+def first_element(first)
+  first [0]
+end
+
+def third_element(third)
+  third [2]
+end
+
+def last_element(last)
+  last [-1]
+end
+
+def first_element_with_array_methods(first_blah_blah)
+  first_blah_blah.first
+end
+
+def last_element_with_array_methods(first_blah_blah)
+  first_blah_blah.last
+end
+
+def length_of_array(long)
+  long.length
+end
