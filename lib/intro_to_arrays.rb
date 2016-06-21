554c800ac38712cfda6d82f63cf56963761d8544
Beth Olmo <betholmo13@gmail.com>
2016-06-21 17:42:15 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  Array.new
+end
+
+def array_with_two_elements
+  array_with_two_elements = ["cats", "dogs"]
+end
+
+def first_element(array)
+  first_element = "Welcome to New York"
+end
+
+def third_element(array)
+  third_element = "Style"
+end
+
+def last_element(array)
+  last_element = "Out of The Woods"
+end
+
+def first_element_with_array_methods(array)
+  array.first
+end
+
+def last_element_with_array_methods(array)
+  array.last
+end
+
+def length_of_array(array)
+  array.count
+end
