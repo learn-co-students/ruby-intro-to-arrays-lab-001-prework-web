d25fa2279855f734ec91b6fc78c6134cd733ab3c
Satu Barnhill <satu.barnhill@gmail.com>
2016-06-26 20:39:49 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,24 @@
+def instantiate_new_array
+  Array.new
+end
+def array_with_two_elements
+  ["", ""]
+end
+def first_element(my_array)
+  my_array[0]
+end
+def third_element(my_array)
+  my_array[2]
+end
+def last_element(my_array)
+  my_array[-1]
+end
+def first_element_with_array_methods(my_array)
+  my_array.first
+end
+def last_element_with_array_methods(my_array)
+  my_array.last
+end
+def length_of_array(my_array)
+  my_array.length
+end
