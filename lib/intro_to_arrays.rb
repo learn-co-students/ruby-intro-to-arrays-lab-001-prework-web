d52c648fdebf4b0e3ce98fc66be000ff5d07f81d
Janelle Rosario <janelle.rosario@gmail.com>
2016-07-10 02:34:14 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  instantiate_new_array = Array.new
+end
+
+def array_with_two_elements
+  array_with_two_elements = Array.new(2)
+end
+
+def first_element(my_first_element)
+  "Welcome to New York"
+end
+
+def third_element(my_third_element)
+  "Style"
+end
+
+def last_element(my_last_element)
+  "Out of The Woods"
+end
+
+def first_element_with_array_methods(first_country)
+  "Thailand"
+end
+
+def last_element_with_array_methods(last_country)
+  "Myanmar"
+end
+
+def length_of_array(length)
+  @programming_languages.length
+end
