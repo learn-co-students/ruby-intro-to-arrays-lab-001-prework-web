992121164a21b07f16c9bec89e1ee176e84d2634
Victoria Melendez <vamelend@gmail.com>
2016-07-11 20:09:23 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,35 @@
+def instantiate_new_array
+ @instantiate_new_array= ["frogs", "Turtles", "lizards"]
+end
+
+def instantiate_new_array
+ []
+end
+
+def array_with_two_elements
+  ["lizard", 423]
+end
+
+def first_element(array)
+  return array[0]
+end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  return array[-1]
+end
+
+def first_element_with_array_methods(array)
+  return array.first
+end
+
+def last_element_with_array_methods(array)
+  return array.last
+end
+
+def length_of_array(array)
+  array.length
+end
