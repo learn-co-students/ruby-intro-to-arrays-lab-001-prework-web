a7eaa89288b9cfce4e832ad7ee997077b9cc860e
aguirrejc <juan.aguirre04@gmail.com>
2016-07-11 00:00:13 -0400

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,38 @@
+def instantiate_new_array
+  @my_new_array = Array.new
+end
+
+def array_with_two_elements
+  ["Welcome to New York", "Blank Space"]
+end
+
+def indexing
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+end
+
+def first_element(array)
+  @taylor_swift = "Welcome to New York"
+end
+
+def third_element(array)
+  @taylor_swift = "Style"
+end
+
+def last_element(array)
+  @taylor_swift = "Out of The Woods"
+end
+
+
+def first_element_with_array_methods(array)
+  array.first
+end
+
+def last_element_with_array_methods(array)
+  array.last
+end
+
+def length_of_array(array)
+  array.length
+end
+
+
