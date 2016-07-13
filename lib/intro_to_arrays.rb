e1795fe6b65ba9b6b1d25a3dcdbfbdf152351048
smcarrera <stephanie.carrera@gmail.com>
2016-07-13 02:09:17 -0400

    intro to arrays lab

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,37 @@
+def instantiate_new_array
+  my_array = []
+end
+
+def array_with_two_elements
+  my_array = ["a", "b"]
+end
+
+def first_element(array)
+  my_array = array
+  element1 = array[0]
+end
+
+def third_element(array)
+  my_array = array
+  element3 = array[2]
+end
+
+def last_element(array)
+  my_array = array
+  element_last = array[-1]
+end
+
+def first_element_with_array_methods(array)
+  my_array = array
+  element_first = array.first
+end
+
+def last_element_with_array_methods(array)
+  my_array = array
+  element_last = array.last
+end
+
+def length_of_array(array)
+  my_array = array
+  element_length = array.length
+end
