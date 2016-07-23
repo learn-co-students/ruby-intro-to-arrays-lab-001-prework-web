b6bfa2ef66220749240733ba1975b74a80936986
craigschiff <craig.schiff@gmail.com>
2016-07-23 14:16:03 -0400

    done

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,37 @@
+def instantiate_new_array
+    array = []
+end
+
+def array_with_two_elements
+    array = ["i", "am"]
+end
+
+def first_element(array)
+    array = ["Welcome to New York", "like", "ruby"]
+    array[0]
+end
+
+def third_element(array)
+    array = ["Welcome to New York", "like", "Style"]
+    array[2]
+end
+
+def last_element(array)
+    array = ["Welcome to New York", "like", "Style", "Out of The Woods"]
+    array[-1]
+end
+
+def first_element_with_array_methods(array)
+    array = ["Thailand", "like", "ruby"]
+    array.first
+end
+
+def last_element_with_array_methods(array)
+    array = ["i", "like", "Myanmar"]
+    array.last
+end
+
+def length_of_array(array)
+    array = ["i", "like", "ruby", "and", "need", "eight", "strings", "done!"]
+    array.count
+end
