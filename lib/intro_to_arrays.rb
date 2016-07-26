c3c747fb9d8246b549c5194bb34df6816f8ad666
hkim53 <paragon.hankido@gmail.com>
2016-07-25 23:22:38 -0400

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,44 @@
+def instantiate_new_array
+  arrays = ["maru","neko","halo"]
+  puts arrays
+end
+
+def instantiate_new_array
+[]
+end
+
+def array_with_two_elements
+  arrays = ["maru","neko"]
+end
+puts array_with_two_elements
+
+def first_element(array)
+  array = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  array.first
+end
+
+def third_element(array)
+  array = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  array[2]
+end
+
+def last_element(array)
+  array = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  array[-1]
+end
+
+def first_element_with_array_methods(south_east_asia)
+  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  south_east_asia.first
+end
+
+def last_element_with_array_methods(south_east_asia)
+  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  south_east_asia.last
+end
+
+def length_of_array(programming_languages)
+  programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+  programming_languages.length
+end
+
