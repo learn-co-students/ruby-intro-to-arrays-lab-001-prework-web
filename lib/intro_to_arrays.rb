c70fc74dcb2721d022119f26e1e94461876801a2
krissasantos <krissa.santos31@gmail.com>
2016-07-29 10:48:48 -0400

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,57 @@
+def instantiate_new_array
+  arr = []
+  return arr
+end
+
+
+def array_with_two_elements
+  arr = [1,2]
+  return arr
+end
+
+
+
+def first_element(taylor_swift)
+
+  return taylor_swift[0]
+
+end
+
+
+def second_element(taylor_swift)
+
+  return taylor_swift[1]
+
+end
+
+
+def third_element(taylor_swift)
+
+  return taylor_swift[2]
+
+end
+
+def last_element(taylor_swift)
+
+  return taylor_swift[-1]
+
+end
+
+def first_element_with_array_methods(taylor_swift) 
+  first = taylor_swift.shift
+  return first
+
+end
+
+def last_element_with_array_methods(taylor_swift) 
+  last = taylor_swift.pop
+  return last
+
+end
+
+
+def length_of_array(taylor_swift) 
+
+  return taylor_swift.length
+end
+
