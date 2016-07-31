2d740cbc3c17e8e94e098e3b642df8e38f047fdf
Jill Berardini <jberardini@gmail.com>
2016-07-31 18:44:36 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,33 @@
+def instantiate_new_array
+  bulgakov_characters = []
+  return bulgakov_characters
+end
+
+def array_with_two_elements
+  bulgakov_characters=["Behemoth", "Koroviev"]
+  return bulgakov_characters
+end
+
+def first_element(array)
+  return array[0]
+end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  return array[-1]
+end
+
+def first_element_with_array_methods(array)
+  return array.first
+end
+
+def last_element_with_array_methods(array)
+  return array.last
+end
+
+def length_of_array(array)
+  return array.size
+end
