530762b8b37a422596c82ca6061f4d4487a37be6
Monica Greco <monicaagreco@gmail.com>
2016-08-29 15:11:43 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+    new_array = []
+end
+
+def array_with_two_elements
+    my_two_array = ["a" ,"b"]
+end
+
+def first_element(my_first_element)
+  my_first_element[0]
+end
+
+def third_element(my_third_element)
+  my_third_element[2]
+end
+
+def last_element(my_last_element)
+  my_last_element[-1]
+end
+
+def first_element_with_array_methods(my_first_element)
+  my_first_element.first
+end
+
+def last_element_with_array_methods(my_last_element)
+  my_last_element.last
+end
+
+def length_of_array(length)
+  length.count
+end
