48ab4dce90a2bebcd0dd275c28fc1e4efc44b14b
npatel111 <nitimaheshpatel@gmail.com>
2016-09-06 17:47:12 -0400

    Done.

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,42 +1,32 @@
 def instantiate_new_array
-  my_new_array = []
+  new_array = []
 end  
 
 def array_with_two_elements
-  my_two_array = [1,2]
+  array = [1,2]
+  array
 end  
 
- taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  
-def first_element (taylor_swift)
-  taylor_swift[0]
+def first_element(array)
+  array[0]
 end
 
-def third_element(taylor_swift)
-  taylor_swift[2]
+def third_element(array)
+  array[2]
 end
 
-def last_element(taylor_swift)
-  taylor_swift[3]
-end    
-
-south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
-
-def first_element_with_array_methods (south_east_asia)
-  south_east_asia.first
+def last_element(array)
+  array[-1]
 end
 
-def last_element_with_array_methods (south_east_asia)
-  south_east_asia.last
+def first_element_with_array_methods(array)
+  array.first      
 end
 
-programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
-def length_of_array(programming_languages)
-  programming_languages.length
+def last_element_with_array_methods(array)
+  array.last
 end
 
-
-
-
-
-
+def length_of_array(array)
+  array.length
+end
